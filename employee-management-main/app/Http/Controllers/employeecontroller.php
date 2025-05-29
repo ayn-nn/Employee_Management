2dcb46787bed85a861a807464044489d57368c05
KazeShigami <elpresidentezerotwo02@gmail.com>
2025-05-29 17:52:45 +0800

    finally

--- employee-management-main/app/Http/Controllers/employeecontroller.php
+++ employee-management-main/app/Http/Controllers/employeecontroller.php
@@ -24,12 +24,12 @@ class employeecontroller extends Controller
     public function store(Request $request)
     {
     $request->validate([
-        'fname' => 'required|max:255|',
-        'lname' => 'required|max:255|',
-        'midname' => 'required|max:255|',
-        'age' => 'required|',
-        'address' => 'required|max:255|',
-        'zip' => 'required|',
+        'fname' => 'required|max:255|string',
+        'lname' => 'required|max:255|string',
+        'midname' => 'required|max:255|string',
+        'age' => 'required|integer',
+        'address' => 'required|max:255|string',
+        'zip' => 'required|integer',
         
     ]);
 
@@ -46,12 +46,12 @@ class employeecontroller extends Controller
     public function update(Request $request, int $id) {
         {
             $request->validate([
-                'fname' => 'required|max:255|',
-                'lname' => 'required|max:255|',
-                'midname' => 'required|max:255|',
-                'age' => 'required|',
-                'address' => 'required|max:255|',
-                'zip' => 'required|',
+                'fname' => 'required|max:255|string',
+                'lname' => 'required|max:255|string',
+                'midname' => 'required|max:255|string',
+                'age' => 'required|integer',
+                'address' => 'required|max:255|string',
+                'zip' => 'required|integer',
                 
             ]);
         
